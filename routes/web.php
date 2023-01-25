c2ef8162b01b7d18383d8a5d5a0d55b21030032d
Andrey Volynkin <kazakmt4@gmail.com>
2023-01-25 14:13:08 +0300

    complete

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Employee;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test_database', function () {
+    $newEmployee = new Employee();
+    $newEmployee->save();
+}
+);
